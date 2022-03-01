5b5e37381ae9d181bc92cf2ffa903f60ae0a489f
Justin Kim <jcykim@csu.fullerton.edu>
2022-02-28 16:41:24 -0800

    practice commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfaQAxiHcHkrz1f1foX5fb3jaNL729BBc",
+  authDomain: "tuffys-trading-post.firebaseapp.com",
+  projectId: "tuffys-trading-post",
+  storageBucket: "tuffys-trading-post.appspot.com",
+  messagingSenderId: "481583233912",
+  appId: "1:481583233912:web:32578c054a8ae83b71268d",
+  measurementId: "G-2SW86JKNL6"
+};
