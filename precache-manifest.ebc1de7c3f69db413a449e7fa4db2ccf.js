8bc9a257f90390be43891bb3dd11e026486163fe
kennethcarbonell <k3nshiro@csu.fullerton.edu>
2022-03-08 10:49:10 -0800

    Updates

---
+++ precache-manifest.ebc1de7c3f69db413a449e7fa4db2ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60d6bbffe567766366a2",
+    "url": "/static/css/main.12de9dfb.chunk.css"
+  },
+  {
+    "revision": "60d6bbffe567766366a2",
+    "url": "/static/js/main.0ee53e0b.chunk.js"
+  },
+  {
+    "revision": "a3041285a02e69056734",
+    "url": "/static/js/runtime~main.01fa5b1e.js"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "03acf47b1c933fdd5274",
+    "url": "/static/js/3.0ad1fd9d.chunk.js"
+  },
+  {
+    "revision": "cfaf181308a05a029ecd60003195772b",
+    "url": "/index.html"
+  }
+];
