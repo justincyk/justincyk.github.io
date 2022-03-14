f02c7fbc41783b6a5a58f3a3c70eefa01f3eaabd
kennethcarbonell <k3nshiro@csu.fullerton.edu>
2022-03-14 15:53:38 -0700

    merged main to main-login and have a working register
    register function is ready and successfully registers on Kenneth's iteration of firebase

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 
 
 function CreateAccount() {
+
+    const history = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const Register = e => {
+      e.preventDefault();
+
+      auth
+          .createUserWithEmailAndPassword(email, password)
+          .then(auth => {
+            if(auth){
+              history('/')
+            }
+          })
+          .catch(error => alert(error.message))
+    }
+    
   return (
     <div className="CreateAccount">
         <form>
             <h5>Username</h5>
-            <input type = "text"/>
+            <input type='text' value = {email} onChange = {e => setEmail(e.target.value)}/>
             <h5>Password</h5>
-            <input type='text'/>
+            <input type='password' value = {password} onChange = {e => setPassword(e.target.value)}/>
             <br/>
 
             {/* links "create" button to login page */}
-            <Link to ="/login">
-                <button>Create</button>
-            </Link>
+                <button type='submit' onClick={Register}>  Create</button>
 
             <br/>
         </form>
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase";
+//import { initializeApp } from 'firebase/app';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
+//This is Kenneth's iteration for firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAfaQAxiHcHkrz1f1foX5fb3jaNL729BBc",
-  authDomain: "tuffys-trading-post.firebaseapp.com",
-  projectId: "tuffys-trading-post",
-  storageBucket: "tuffys-trading-post.appspot.com",
-  messagingSenderId: "481583233912",
-  appId: "1:481583233912:web:32578c054a8ae83b71268d",
-  measurementId: "G-2SW86JKNL6"
+  apiKey: "AIzaSyB9vmZAOEhKlv75elnQLtRMl-ewPfoNMv0",
+  authDomain: "tuffy-s-trading-post.firebaseapp.com",
+  projectId: "tuffy-s-trading-post",
+  storageBucket: "tuffy-s-trading-post.appspot.com",
+  messagingSenderId: "786932770036",
+  appId: "1:786932770036:web:10b4714e8e44274bd56aa1",
+  measurementId: "G-EVNHLNLVPS"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
